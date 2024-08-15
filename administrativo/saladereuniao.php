b04f1d67cd54ed5e3a03eddf83059604ea5a0fe6
KarlosFodaoBr <johndoe@example.com>
2024-08-15 20:00:01 -0300

    Mudei o titulo da sala de reuniao

--- administrativo/saladereuniao.php
+++ administrativo/saladereuniao.php
@@ -27,7 +27,7 @@ if (!$_SESSION['poder'] > 100) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Reuniao</title>
     <link href="/naoeindex/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
