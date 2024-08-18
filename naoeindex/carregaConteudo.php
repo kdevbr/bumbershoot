f52686303ab6080a7a4065ccca176bde3b9530c9
KarlosFodaoBr <johndoe@example.com>
2024-08-18 16:52:38 -0300

    Resolvi bugs de btn, comecei a fazer front do sistema de paginaçao

--- naoeindex/carregaConteudo.php
+++ naoeindex/carregaConteudo.php
@@ -1,24 +1,29 @@
 <?php
+session_start();
 $url = $_GET['page'];
 //$tipo = $_GET['tipo'];
 
 setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
 date_default_timezone_set('America/Sao_Paulo');
+$dados = [
+    'titulo' => 'teste Nº 9108',
+    'data' => ''.date('d/m/Y'),
+    'autor' => 'eu',
+    'conteudo' => 'Voce esta em: bumbershoot.com.br'.$url,
+    'tipo'=> 'link'
+];
+
 if($url == "/desconectar"){
     unset($_SESSION['logade']);
     unset($_SESSION['userId']);
     unset($_SESSION['poder']);
-    echo 'asd';
+    unset($_COOKIE['lembrarDeMim']);
+    setcookie('lembrarDeMim', '', -1, '/');
+    $dados['Refrache'] = true;
 }
-echo $url;
-$dados = [
-'titulo' => 'teste Nº 9108',
-'data' => ''.date('d/m/Y'),
-'autor' => 'eu',
-'conteudo' => 'Voce esta em: bumbershoot.com.br'.$url,
-'tipo'=> 'link'
-];
-
+if($url ==  "/administrativo"){
+    $dados['ADM'] = true;
+}
 
 $jsonData = json_encode($dados);
 
