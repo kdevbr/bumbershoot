172046ded08aa37b9567c32463609b6dbf7c7641
KarlosFodaoBr <johndoe@example.com>
2025-03-23 15:30:13 -0300

    Adiciona suporte ao Pusher com configuração inicial e arquivos necessários

---
+++ Pusher Channels/index.php
@@ -0,0 +1,18 @@
+<?php
+ require __DIR__ . '/../vendor/autoload.php';
+
+  $options = array(
+    'cluster' => 'sa1',
+    'useTLS' => true
+  );
+  $pusher = new Pusher\Pusher(
+    '44b4d3f9ece01dfa8fb6',
+    '1f3618851d79f86007ee',
+    '1952164',
+    $options
+  );
+
+
+$pusher->trigger('presence-geraldo', 'zap', array('message' => $_POST['msg'], 'username' => $_POST['username']));
+
+?>
