feea72e246d8eb298a1540854ff9fa9e5f215130
KarlosFodaoBr <johndoe@example.com>
2025-12-22 01:07:43 -0300

    Atualiza configurações de banco de dados e melhora a lógica de interação no jogo

--- nova pasta/hole.js
+++ nova pasta/hole.js
@@ -1,32 +1,63 @@
 let som = document.getElementById("thunder")
 
+let childrens = document.querySelectorAll('.son')
 let thunders = document.querySelectorAll('img')
-window.addEventListener('keypress',(x)=>{
- if (x.code  === 'KeyH') {
-    teclar()
-  }
-  if (x.code  === 'KeyY') {
-    teclar()
-  }
-  console.log(x.code, x.key)
-})
+let dad = document.getElementById('dad')
 
+let selecionado = 0;
+let maximo = childrens.length - 1;
+
+dad.style.transform = `translateX(calc(-${160*selecionado+80}px + 50%))`
+
+window.addEventListener('keypress',(x)=>{
+ if (x.code  === 'KeyA') {
+    
+    if(selecionado > 0){
+      selecionado--;
+      dad.style.transform = `translateX(calc(-${160*selecionado+80}px + 50%))`
+    }
+    teclar()
+  }
+  if (x.code  === 'KeyD') {
+    
+    if(selecionado < maximo){
+      selecionado++;
+      dad.style.transform = `translateX(calc(-${160*selecionado+80}px + 50%))`
+    }
+    teclar()
+  }
+console.log(selecionado)
+})
+teclar()
 function teclar(){
-  console.log("TURAMMM!")
+
   som.currentTime = 0
   som.play();
-
+ 
   thunders.forEach((x)=>{
 
    x.style.opacity = "1"
-
   })
   
   setTimeout (()=>{
     thunders.forEach((x)=>{
+      x.style.opacity = "0"})
+    },100)
 
-   x.style.opacity = "0"})
-
-  },100)
-  
+      childrens.forEach((x)=>{
+        x.classList.remove('rabbitEnteredTheHole')
+      })
+      childrens[selecionado].classList.add('rabbitEnteredTheHole')
+      
 }
+
+/*let childrens = document.querySelectorAll('.son')
+
+
+
+/*
+setInterval(() => {
+ childrens[1].classList.toggle('rabbitEnteredTheHole')
+ childrens[2].classList.toggle('rabbitEnteredTheHole')
+}, 2000);
+*/
