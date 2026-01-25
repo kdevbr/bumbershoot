6aa743e3ac2e36b3854a049f64a9d4bc5dbf1784
KarlosFodaoBr <johndoe@example.com>
2026-01-25 12:17:06 -0300

    Remove arquivos desnecessários, incluindo bd.php, GETDATA.php, POSTDATA.php e index.html

--- daoo/bd.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "x14.x10hosting.com";
-$username = "mgivajkb_dao"; 
-$password = "12345678";
-$dbname = "mgivajkb_dao";
-$port = 3306;
-
-// Conectar ao banco de dados
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
